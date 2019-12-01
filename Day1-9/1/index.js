bb005b2406a6c91c9ec5be84295a57cc949f306a
Benjamin Joham <bjoham.mmt-b2017@fh-salzburg.ac.at>
2019-12-01 19:13:16 +0100

    day1 - task2

--- Day1-9/1/index.js
+++ Day1-9/1/index.js
@@ -3,7 +3,7 @@ const util = require("util");
 
 const readFile = util.promisify(fs.readFile);
 
-async function init() {
+async function day1() {
   let data;
   try {
     const buffer = await readFile("./input.txt", "utf8");
@@ -14,10 +14,15 @@ async function init() {
   if (data !== undefined) {
     let sum = 0;
     for (const item of data) {
-      sum += Math.floor(item / 3) - 2;
+      let fuelToAdd = item;
+      while (fuelToAdd > 0) {
+        fuelToAdd = Math.floor(fuelToAdd / 3) - 2;
+        if (fuelToAdd <= 0) break;
+        sum += fuelToAdd;
+      }
     }
     console.log("sum", sum);
   }
 }
 
-init();
+day1();
