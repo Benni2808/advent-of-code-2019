9af6a3bcb22ce000f52fc5a90a4d524c45d44d22
Benjamin Joham <bjoham.mmt-b2017@fh-salzburg.ac.at>
2019-12-02 22:06:18 +0100

    day2 - task2

--- Day1-9/2/index.js
+++ Day1-9/2/index.js
@@ -1,24 +1,44 @@
-const { input } = require("./input")
-const tmp = [...input]
-tmp[1] = 12
-tmp[2] = 2
-let index = 0
-let halt = false
-while (!halt) {
-  switch (tmp[index]) {
-    case 1:
-      tmp[tmp[index + 3]] = tmp[tmp[index + 1]] + tmp[tmp[index + 2]]
-      index += 4
-      break
-    case 2:
-      tmp[tmp[index + 3]] = tmp[tmp[index + 1]] * tmp[tmp[index + 2]]
-      index += 4
-      break
-    case 99:
-      halt = true
-      break
-    default:
-      console.log("somethinig went wrong at index", index, tmp[index])
+const { inputTask1, inputTask2 } = require("./input")
+const secret = 19690720
+
+console.log("task 1", run())
+console.log("task 2", task2())
+function run(codes = [...inputTask1], noun = 12, verb = 2) {
+  let halt = false
+  // console.log("run", noun, verb)
+  let index = 0
+  codes[1] = noun
+  codes[2] = verb
+  while (!halt) {
+    switch (codes[index]) {
+      case 1:
+        codes[codes[index + 3]] =
+          codes[codes[index + 1]] + codes[codes[index + 2]]
+        index += 4
+        break
+      case 2:
+        codes[codes[index + 3]] =
+          codes[codes[index + 1]] * codes[codes[index + 2]]
+        index += 4
+        break
+      case 99:
+        halt = true
+        return codes[0]
+      default:
+        console.log("somethinig went wrong at index", index, codes[index])
+    }
   }
 }
-console.log("output", tmp[0])
+
+function task2() {
+  for (let verb = 0; verb < 100; verb++) {
+    for (let noun = 0; noun < 100; noun++) {
+      tmp = [...inputTask2]
+      tmp[1] = verb
+      tmp[2] = noun
+      result = run(tmp, noun, verb)
+      // console.log("subtotal", result)
+      if (result === secret) return 100 * noun + verb
+    }
+  }
+}
